b8f4b964bbe7dafbe3949e8818c38cf7cc7e118b
Araik <iaraik99@gmail.com>
2022-05-10 16:04:34 +0900

    Добавил тестовую среду для dll

---
+++ TestASCIIConvertor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace TestASCIIConvertor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                 Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            Console.WriteLine("Нажмите Enter, чтобы начать...\n");
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+
+            }
+        }
+    }
+}
