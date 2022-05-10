afc33befe0721ebeedf21ef1ba3a8a76ade78a3b
Araik <iaraik99@gmail.com>
2022-05-10 11:07:58 +0900

    Настройка решения

---
+++ ASCIICamera/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASCIICamera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ASCIIConvertor/ASCIIConvertor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ASCIIConvertor
+{
+    public class ASCIIConvertor
+    {
+
+    }
+}
