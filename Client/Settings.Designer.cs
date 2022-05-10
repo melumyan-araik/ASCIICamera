d38a26406ea298fb41afc4089777322171411055
Araik <iaraik99@gmail.com>
2022-05-10 20:04:41 +0900

    SettingsForm готово

--- Client/Settings.Designer.cs
+++ Client/Settings.Designer.cs
@@ -32,31 +32,105 @@ namespace Client
         private void InitializeComponent()
         {
             this.buttonCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxIp = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.errorPort = new System.Windows.Forms.Label();
+            this.errorIp = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonCancel
             // 
-            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.buttonCancel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.buttonCancel.ForeColor = System.Drawing.SystemColors.Control;
-            this.buttonCancel.Location = new System.Drawing.Point(230, 262);
+            this.buttonCancel.Location = new System.Drawing.Point(12, 156);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(84, 31);
-            this.buttonCancel.TabIndex = 0;
-            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.Size = new System.Drawing.Size(119, 41);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Отменить";
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP адрес";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Порт";
+            // 
+            // textBoxIp
+            // 
+            this.textBoxIp.Location = new System.Drawing.Point(69, 24);
+            this.textBoxIp.Name = "textBoxIp";
+            this.textBoxIp.Size = new System.Drawing.Size(197, 23);
+            this.textBoxIp.TabIndex = 4;
+            this.textBoxIp.Enter += new System.EventHandler(this.textBoxIp_Enter);
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(69, 89);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(197, 23);
+            this.textBoxPort.TabIndex = 5;
+            this.textBoxPort.Enter += new System.EventHandler(this.textBoxPort_Enter);
+            this.textBoxPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPort_KeyPress);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(147, 156);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(119, 41);
+            this.buttonOk.TabIndex = 6;
+            this.buttonOk.Text = "Применить";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // errorPort
+            // 
+            this.errorPort.AutoSize = true;
+            this.errorPort.ForeColor = System.Drawing.Color.Red;
+            this.errorPort.Location = new System.Drawing.Point(69, 115);
+            this.errorPort.Name = "errorPort";
+            this.errorPort.Size = new System.Drawing.Size(0, 15);
+            this.errorPort.TabIndex = 7;
+            // 
+            // errorIp
+            // 
+            this.errorIp.AutoSize = true;
+            this.errorIp.ForeColor = System.Drawing.Color.Red;
+            this.errorIp.Location = new System.Drawing.Point(69, 50);
+            this.errorIp.Name = "errorIp";
+            this.errorIp.Size = new System.Drawing.Size(0, 15);
+            this.errorIp.TabIndex = 8;
             // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.CausesValidation = false;
-            this.ClientSize = new System.Drawing.Size(361, 350);
+            this.ClientSize = new System.Drawing.Size(278, 209);
+            this.Controls.Add(this.errorIp);
+            this.Controls.Add(this.errorPort);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonCancel);
             this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Settings";
@@ -64,11 +138,19 @@ namespace Client
             this.Text = "Настройки";
             this.Load += new System.EventHandler(this.Settings_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button buttonCancel;
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxIp;
+        private TextBox textBoxPort;
+        private Button buttonOk;
+        private Label errorPort;
+        private Label errorIp;
     }
 }
--- Client/Settings.cs
+++ Client/Settings.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Client
@@ -17,12 +19,78 @@ namespace Client
 
         private void Settings_Load(object sender, EventArgs e)
         {
-
+            var socket = ConfigurationManager.AppSettings.Get("socket");
+            if (socket != null)
+            {
+                textBoxIp.Text = socket.Split(':')[0];
+                textBoxPort.Text = socket.Split(':')[1];
+            }
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            var checkIp = false;
+            var checkPort = false;
+            Regex regIP = new Regex(@"\b(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])\b");
+            if (regIP.IsMatch(textBoxIp.Text))
+            {
+                checkIp = true;
+            }
+            else
+            {
+                errorIp.Text = "Неверный Ip";
+            }
+
+            if (int.Parse(textBoxPort.Text) >= 0 && int.Parse(textBoxPort.Text) < 65536)
+            {
+                checkPort = true;
+            }
+            else
+            {
+                errorPort.Text = "Неверный порт";
+            }
+
+            if (checkPort && checkIp)
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings["socket"] == null)
+                {
+                    settings.Add("socket", $"{textBoxIp.Text}:{textBoxPort.Text}");
+                }
+                else
+                {
+                    settings["socket"].Value = $"{textBoxIp.Text}:{textBoxPort.Text}";
+                }
+
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+
+                Close();
+            }
+        }
+
+        private void textBoxPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxIp_Enter(object sender, EventArgs e)
+        {
+            errorIp.Text = "";
+        }
+
+        private void textBoxPort_Enter(object sender, EventArgs e)
+        {
+            errorPort.Text = "";
         }
     }
 }
