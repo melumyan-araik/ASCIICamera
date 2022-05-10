6797ed7fb0714620393e36fcefdcc81beedc7f38
Araik <iaraik99@gmail.com>
2022-05-10 12:11:40 +0900

    Добавил методы OpenImg, ResizeBitmap

--- ASCIIConvertor/ASCIIConvertor.cs
+++ ASCIIConvertor/ASCIIConvertor.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 
 namespace ASCIIConvertor
 {
-    public class ASCIIConvertor
+    public class ASCIIConvertor : IASSCIIConvertor
     {
+        public static readonly string[] IMG_EXTENTION = { ".bmp", ".png", ".jpg", ".JPEG" };
 
+        public Bitmap OpenImg(string path)
+        {
+            try
+            {
+                var exten = Path.GetExtension(path);
+                if (!CheckExtention(exten, IMG_EXTENTION))
+                    throw new Exception("Тип не поддерживается!");
+                return new Bitmap(path);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Bitmap ResizeBitmap(Bitmap bitmapBegin, int maxWidth, double widthOffset = 1.5)
+        {
+            var newHeight = bitmapBegin.Height / widthOffset * maxWidth / bitmapBegin.Width;
+            if (bitmapBegin.Width > maxWidth || bitmapBegin.Height > newHeight)
+                bitmapBegin = new Bitmap(bitmapBegin, new Size(maxWidth, (int)newHeight));
+            return bitmapBegin;
+        }
+
+        private bool CheckExtention(string ext, string[] extentionArr)
+        {
+            foreach (var item in extentionArr)
+            {
+                if (ext == item)
+                    return true;
+            }
+            return false;
+        }
     }
 }
---
+++ ASCIIConvertor/IASSCIIConvertor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ASCIIConvertor
+{
+    interface IASSCIIConvertor
+    {
+        /// <summary>
+        /// Открытие картинки
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public Bitmap OpenImg(string path);
+
+        /// <summary>
+        /// Изменение размера картинки по ширине
+        /// </summary>
+        /// <param name="bitmapBegin"></param>
+        /// <param name="maxWidth"></param>
+        /// <param name="widthOffset"></param>
+        /// <returns></returns>
+        Bitmap ResizeBitmap(Bitmap bitmapBegin, int maxWidth, double widthOffset);
+    }
+}
